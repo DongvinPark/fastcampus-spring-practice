e94a1b953bcd3427bd0ed867278862ac5e005d75
Dongvin,Park <dongvin99@naver.com>
2022-04-14 14:38:18 +0900

    Change #1 - package
    이전 프로젝트에서 그대로 복사함

--- src/test/java/com/fastcampus/springpractice/logic/BubbleSortTest.java
+++ src/test/java/com/fastcampus/springpractice/logic/BubbleSortTest.java
@@ -1,4 +1,4 @@
-package com.fastcampus.javaoop.logic;
+package com.fastcampus.springpractice.logic;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,16 +7,16 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class JavaSortTest {
+class BubbleSortTest {
 
-    @DisplayName("자바소트 - 리스트를 넣으면 정렬된 결과를 보여줌")
+    @DisplayName("버블소트 - 리스트를 넣으면 정렬된 결과를 보여줌")
     @Test
     void given_List_WhenExecuting_ThenReturnSortedList(){
         //given
-        JavaSort<Integer> javaSort = new JavaSort<>();
+        BubbleSort<Integer> bubbleSort = new BubbleSort<>();
 
         //when
-        List<Integer> actual = javaSort.sort(List.of(3,2,4,5,1));
+        List<Integer> actual =bubbleSort.sort(List.of(3,2,4,5,1));
 
         //then
         assertEquals(List.of(1,2,3,4,5), actual);
